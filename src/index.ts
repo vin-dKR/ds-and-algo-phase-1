51e9ad618c45216f7c1196961eda82ed921307d8
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-10-19 13:15:10 +0530

    chap3- linkedlist

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { towerOfHanoi } from "../topics/res-and-backtracking/1-tower-of-hanoi";
-import { kAryStringGen } from "../topics/res-and-backtracking/4-k-ary-string-gen";
+import { LinkedList } from "../topics/3-linked-list/0-ll";
 
 {/*
+    import { towerOfHanoi } from "../topics/res-and-backtracking/1-tower-of-hanoi";
+    import { kAryStringGen } from "../topics/res-and-backtracking/4-k-ary-string-gen";
     import { startBitString } from "../topics/res-and-backtracking/3-gen-str-of-nbits";
     import { towerOfHanoi } from "../topics/res-and-backtracking/1-tower-of-hanoi";
     import { isSortedArray } from "../topics/res-and-backtracking/2-array-sort";
@@ -10,7 +11,14 @@ import { kAryStringGen } from "../topics/res-and-backtracking/4-k-ary-string-gen
 {/*
     const xxx = towerOfHanoi(3, "A", "B", "C")
     const isSorted = isSortedArray([0, 1, 4, 8, 9])
+    const arr = startBitString(4)
 */}
 
-const arr = towerOfHanoi(3, 'A', 'B', 'C')
-console.log(arr)
+const list = new LinkedList()
+
+list.append(1)
+list.append(2)
+list.insertAtBeg(0)
+list.insertAtBeg(-1)
+
+console.log()
